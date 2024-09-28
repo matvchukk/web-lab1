e323346c054c6314341a740733e5a4535fb68f32
Anastasiia Matviichuk <a.matviychuk0831@gmail.com>
2024-09-28 16:13:19 +0300

    Add feature 1

--- src/main/java/com/lab1/weblab1/WebLab1Application.java
+++ src/main/java/com/lab1/weblab1/WebLab1Application.java
@@ -8,6 +8,7 @@ public class WebLab1Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hiiii!");
+		System.out.println("How r u?");
 		SpringApplication.run(WebLab1Application.class, args);
 	}
 
