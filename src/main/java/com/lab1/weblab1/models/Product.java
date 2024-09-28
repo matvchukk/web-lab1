4ee02f4e724be2472009fdcfe65e41f107721b55
Anastasiia Matviichuk <a.matviychuk0831@gmail.com>
2024-09-28 14:40:46 +0300

    Fix model

--- src/main/java/com/lab1/weblab1/models/Product.java
+++ src/main/java/com/lab1/weblab1/models/Product.java
@@ -3,6 +3,7 @@ package com.lab1.weblab1.models;
 public class Product {
     private int id;
     private String name;
+    private double price;
 
     public Product (int id, String name) {
         this.id = id;
